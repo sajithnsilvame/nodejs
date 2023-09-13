77d563a5bac49f5dcb781c0468d4ca3f85beaa95
Sajith N Silva <sajithnsilva.me@gmail.com>
2023-09-13 12:34:45 +0530

    first commit

---
+++ MathFun.js
@@ -0,0 +1,20 @@
+export const plus = (num1,num2) =>{
+  const value = num1+num2
+  return value
+}
+
+export const minus = (num1,num2) =>{
+  const value = num1-num2
+  return value
+}
+
+export const multiple = (num1,num2) =>{
+  const value = num1*num2
+  return value
+}
+
+export const devided = (num1,num2) =>{
+  const value = num1/num2
+  return value
+}
+
---
+++ index.js
@@ -0,0 +1,29 @@
+
+import { log } from "node:console";
+import {dirname,join} from "node:path";
+import { readFile, writeFile } from "node:fs";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename)
+
+
+readFile(join(__dirname,'mytext.txt'), 'utf-8', (err,data) => {
+  if(!err){
+    console.log(data.toString())
+  }else{
+    console.log(err)
+  }
+})
+const data = "Sajith is a software engineer"
+const data2 = "Sajith is a fullstack developer"
+writeFile(join(__dirname,'write2.txt'), data2, (err) =>{
+  if(err)
+  { 
+    log(err.message()) 
+  }
+  else
+  { 
+    log(data2) 
+  }
+})
